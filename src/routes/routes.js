a70cc18125916acd631a988ed33d66beab6c8b6c
huy.ltn204986 <huy.ltn204986@sis.hust.edu.vn>
2023-08-27 11:25:21 +0700

    sidebar #1

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -6,11 +6,13 @@ import Following from '~/pages/Following';
 import Profile from '~/pages/Profile';
 import Upload from '~/pages/Upload';
 import Search from '~/pages/Search';
+import Live from '~/pages/Live';
 
 // không cần đăng nhập vẫn xem đc
 const publicRoutes = [
     { path: config.routes.home, component: Home },
     { path: config.routes.following, component: Following },
+    { path: config.routes.live, component: Live },
     { path: config.routes.profile, component: Profile },
     { path: config.routes.search, component: Search, layout: null },
     { path: config.routes.upload, component: Upload, layout: HeaderOnly },
